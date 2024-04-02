44684505703c5dd3db153ffa4a60056cc20d314d
Pratik Goswami <prtkgoswami8@gmail.com>
2024-04-02 08:44:47 -0700

    Random Bug Fixes

--- src/app/api/checkSecret/route.ts
+++ src/app/api/checkSecret/route.ts
@@ -1,13 +1,16 @@
 import bcrypt from "bcrypt";
 
-const SECRET_HASH =
-  "$2b$10$mDqBuxX8ETeigaVCsteu6..H6VXbBJaKa8l24O1y7EGkFcvVdspCm";
-
 export async function POST(request: Request) {
   const reqParams = await request.json();
   const { secretStr } = reqParams;
   try {
-    const match = await bcrypt.compare(secretStr, SECRET_HASH);
-    return Response.json({ match });
+    if (process.env.NEXT_PUBLIC_SECRET_HASH) {
+      const match = await bcrypt.compare(
+        secretStr,
+        process.env.NEXT_PUBLIC_SECRET_HASH
+      );
+      return Response.json({ match });
+    }
+    throw new Error("Secret Hash not defined");
   } catch (error) {}
 }
