1f465507af5946cdce35c76116d42498cca7d2fe
Pratik Goswami <prtkgoswami8@gmail.com>
2025-12-09 12:11:50 +0530

    Build Bug Fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      if (Array.isArray(config.externals)) {
+        config.externals.push("jsdom", "canvas");
+      } else {
+        config.externals = config.externals || [];
+        config.externals.push("jsdom", "canvas");
+      }
+    }
+    return config;
+  },
   images: {
     remotePatterns: [
       {
